5edb714c2f51efb6b01decee831f951a4fbdd733
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-09-08 18:28:54 +0100

    updated

--- spikes/4_dense_optical_flow/4.py
+++ spikes/4_dense_optical_flow/4.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture("V2.py")
+
+cap = cv2.VideoCapture("V2.mp4")
 
 
 # See http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_video/py_lucas_kanade/py_lucas_kanade.html
@@ -14,7 +15,7 @@ while(1):
     ret, frame2 = cap.read()
     next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
 
-    flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3.0, 15.0, 3.0, 5.0, 1.2, 0.0)
 
     mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
     hsv[...,0] = ang*180/np.pi/2
